662e162dc4d7a6e53fa35d9248faa44b7cba4a25
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2022-12-01 08:11:12 +0300

    first commit

---
+++ components/banner.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      isVisible: true,
+    }
+  }
+}
+</script>
+<template>
+<section v-show="isVisible">
+    <img src="https://kigili.akinoncdn.com/cms/2022/11/28/3f7ece87-a51b-46e6-91ab-bd129b2823b4.png" alt="">
+</section>
+</template>
+
+<style scoped>
+img{
+    margin-top: 18px;
+}
+</style>
